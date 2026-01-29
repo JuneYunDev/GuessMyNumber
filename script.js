ae948ec159b0147eb6759011da63157cf309d010
June Yun <97yunjy@gmail.com>
2026-01-28 22:38:50 -0700

    Question mark setting for game

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ console.log(document.querySelector('.guess').value);*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,7 +22,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //When player win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -66,6 +65,7 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('.number').textContent = '?';
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
